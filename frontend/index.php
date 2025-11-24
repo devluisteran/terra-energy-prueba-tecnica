11a5d813af81e5d505f121946ebded4f06706876
toy <devluisteran@gmail.com>
2025-11-23 18:56:48 -0600

    new case view main

---
+++ frontend/index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./styles/index.css">
+    <title>Tasks</title>
+</head>
+
+<body>
+    <div class="container">
+        <h1 class="title">Tasks</h1>
+        <div>
+            <button class="btn-primary">Add Task</button>
+        </div>
+        <div class="card">
+            <div class="div-table">
+                <table id="table">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Name Task</th>
+                            <th>Description</th>
+                            <th>Created at</th>
+                            <th>delivery date</th>
+                            <th>Options</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>Tarea 1</td>
+                            <td>Descripcion 1</td>
+                            <td>23/11/2025</td>
+                            <td>28/11/2025</td>
+                            <td>
+                                <div class="button-group">
+                                    <button class="btn-edit">Edit</button>
+                                    <button class="btn-delete">Delete</button>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>1</td>
+                            <td>Tarea 1</td>
+                            <td>Descripcion 1</td>
+                            <td>23/11/2025</td>
+                            <td>28/11/2025</td>
+                            <td>
+                                <div class="button-group">
+                                    <button class="btn-edit">Edit</button>
+                                    <button class="btn-delete">Delete</button>
+                                </div>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+
+    </div>
+</body>
+
+</html>
